d81adef498981bffccce8e49c7e8a2ae0f13aa7f
Mouradouchane <mouradouchane@gmail.com>
2024-04-14 15:20:46 +0100

    drop fantasy and back to reality

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 
-#define OS WIN
+#include <iostream>
 #include "s_files.hpp"
 
 int main(){
 
-    file empty_file();
-    
-    new_file new_zip("new_file.zip", 24mb);
+    // read file test
+    file txt = s_files::read_from_file("tar.txt");
 
-    file py_script("./src/mod.py" , file::read);
-    file cfg_script("../config.cfg" , file::write);
-    file md_file("c://dir1/dir2/readme.md" , file::read_and_write);
+    if(txt.buffer == nullptr){
+        std::cout << "failed to read tar.txt\n";
+    }
+    else {
+        std::cout << "tar.txt :\n";
+        char* data = (char*)txt.buffer;
 
+        for(uint32_t i = 0 ; i < txt.size ; i++){
+            std::cout << data[i];
+        }
+    }
+
+    // write to file test
+    if( s_files::write_to_file("tar.txt",(void*)&"few lines",8,true)){
+        std::cout << "write successed \n";
+    }
+    else {
+        std::cout << "write failed \n";
+    }
 
     return 0;
 }
--- s_files.hpp
+++ s_files.hpp
@@ -2,108 +2,26 @@
 #ifndef S_FILES_LIB_HPP
 #define S_FILES_LIB_HPP
 
+#include <errno.h>
+#include <stdio.h>
+#include <cstdio>
 #include <string>
 
-/*
-    ========== library types ========== 
-*/
-class file;
-class atomic_file;
-class file_information;
-// ====================================
-
-
-class file_information { // represent file information and deatils 
-
-private :
-    // file attribute
-    uint32_t _attribute = NULL;
-    uint32_t _number_of_links = NULL;
-    uint32_t _volume_serial_number = NULL;
-
-    uint64_t _creation_time = NULL;
-    uint64_t _last_read_time = NULL;
-    uint64_t _last_wrtie_time = NULL;
-
-    uint32_t _size_low  = NULL;
-    uint32_t _size_high = NULL;
-    
-    uint32_t _index_low  = NULL;
-    uint32_t _index_high = NULL;
-
-public : 
-    // constructor's
-    file_information();
-
-    // destructor
-    ~file_information();
+typedef struct file{
+    uint32_t size = 0;
+    void* buffer  = nullptr;
 };
-// class file_information "end"
 
+// main namesapce =============================
+namespace s_files{
 
-/*
-    to represent files as objects in the code
-*/
-class file {
-
-public :
-    static const char read_and_write = 'rw';
-    static const char read    = 'r';
-    static const char write   = 'w';
-    static const char binary  = 'b';
-    static const char special = 's';
+    file read_from_file(std::string const& file_path, bool read_as_binary = true);
+    bool write_to_file(std::string const& file_path, void* buffer, uint32_t size, bool append = false);
     
-private :
-    std::string _name = "";
-    std::string _type = ""; // file.type : bin,rar,...
+    bool create_file(std::string const& file_path);
+    bool delete_file(std::string const& file_path);
 
-    std::string _current_path = ""; // current path of this file "path without name"
-    std::string _current_full_path = ""; // full path with everything
-    std::string _new_path = ""; // new path where to save this file "path without name"
-    std::string _new_full_path = ""; // new path where to save this file "path without name"
-
-    uint64_t _size = NULL; // file size in byte
-    std::string _permissions = "";
-
-    void* data = nullptr;
-
-    bool _lock = false; // for multi-threading
-
-public :
-
-    // public object contain deatils and information about this files 
-    file_information deatiles();
-
-    // constructor's
-    file();
-    file(std::string file_path);
-    file(std::string file_path , std::string file_permission);
-
-    // destructor
-    ~file();
-
-    // file public functions 
-
-    std::string name(); // o(1)
-    std::string type(); // o(1)
-
-    void rename(std::string const& new_name); // o(1)
-    void change_type(std::string const& new_type); // o(1)
-
-    std::string path(); // o(1)
-    std::string full_path(); // o(1)
-
-    uint64_t size(); // o(1)
-
-    std::string permissions(); // o(1)
-
-    // atomic stuff
-    void lock(); // o(1)
-    void unlock(); // o(1)
-    bool is_locked(); // o(1)
-    bool try_lock(); // o(1)
-
-};
-// class file "end"
+}
+// s_files namespace end =====================
 
 #endif
