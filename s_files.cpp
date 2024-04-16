09bc4272e9a0a0779b301b6626763f93d1b399c3
Mouradouchane <mouradouchane@gmail.com>
2024-04-16 02:53:03 +0100

    add:open write create delete function
    new function open write create delete tested and works fine

--- s_files.cpp
+++ s_files.cpp
@@ -38,11 +38,11 @@ file s_files::read_from_file(
 }
 
 bool s_files::write_to_file(
-    std::string const& file_path, void* buffer , uint32_t size, bool append
+    std::string const& file_path, void* buffer, uint32_t size, bool append
 ){
     
     // try to open file
-    std::FILE* handle = nullptr; 
+    std::FILE* handle = nullptr;
     handle = fopen(file_path.c_str() , (append ? "a" : "w") );
 
     // try to write to file
@@ -60,5 +60,43 @@ bool s_files::write_to_file(
     return false;
 }
 
-	
+bool s_files::write_to_file(
+    std::FILE* file_pointer, void* buffer, uint32_t size, bool append
+){
+    
+    // try to write to file
+    if(file_pointer != nullptr) {
+        size_t write_result = NULL;
+        write_result = fwrite(buffer , 1 , size , file_pointer);
+        
+        return (write_result != NULL);
+    }
+    
+    return false;
+}
+
+
+std::FILE* s_files::open_file(std::string const& file_path , std::string const& permission){
+     
+    // try to open file
+    std::FILE* handle = nullptr; 
+    handle = fopen(file_path.c_str() , permission.c_str());
+
+    return handle;
+}
+
+std::FILE* s_files::create_file(std::string const& file_path ){
+     
+    // try to open file
+    std::FILE* handle = nullptr; 
+    handle = fopen(file_path.c_str() , "w");
+
+    return handle;
+}
+
+bool s_files::delete_file(std::string const& file_path){
+    // int remove(const char *filename)
+   return (remove(file_path.c_str()) == 0);
+}
+
 #endif
--- s_files.hpp
+++ s_files.hpp
@@ -9,16 +9,19 @@
 
 typedef struct file{
     uint32_t size = 0;
-    void* buffer  = nullptr;
+    void*  buffer = nullptr;
 };
 
 // main namesapce =============================
 namespace s_files{
 
-    file read_from_file(std::string const& file_path, bool read_as_binary = true);
-    bool write_to_file(std::string const& file_path, void* buffer, uint32_t size, bool append = false);
+    file read_from_file(std::string const& file_path, bool read_as_binary);
+    std::FILE* open_file(std::string const& file_path , std::string const& permission);
     
-    bool create_file(std::string const& file_path);
+    bool write_to_file(std::string const& file_path, void* buffer, uint32_t size, bool append);
+    bool write_to_file(std::FILE* file_pointer, void* buffer, uint32_t size, bool append);
+    
+    std::FILE* create_file(std::string const& file_path);
     bool delete_file(std::string const& file_path);
 
 }
